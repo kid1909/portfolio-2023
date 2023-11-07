2948232fbbea224608d892db172e7a8b342dc760
Quang Vu <ryan19091990@gmail.com>
2023-11-07 15:40:44 -0800

    fixing project moving section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import SectionTitle from './SectionTitle'
 
 const Projects = () => {
   return (
-    <section className="py-20 align-element id='projects">
+    <section className="py-20 align-element" id="projects">
       <SectionTitle text="web creations" />
       <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {
